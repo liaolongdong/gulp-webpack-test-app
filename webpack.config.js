8fad660505a59637fee28588dc652f19ae92e1d4
liaolongdong <liaolongdong@diandian-tech.com>
2016-08-03 15:38:02 +0800

    react hello world

--- webpack.config.js
+++ webpack.config.js
@@ -33,25 +33,43 @@ const webpack = require('webpack');
 // 	}
 // }
 
+// module.exports = {
+// 	entry: {
+// 		bundle1: './main.js',
+// 		bundle2: ['./entry1.js','./entry2.js']
+// 	},
+// 	output: {
+// 		path: __dirname + '/bin',
+// 		filename: '[name].js'
+// 	},
+// 	module: {
+// 		loaders: [
+// 			{
+// 				test: /\.js$/,
+// 				exclude: /node_modules/,
+// 				loader: 'babel',
+// 				query: {
+// 					presets: ['es2015']
+// 				}
+// 			}
+// 		]
+// 	}
+// };
+
 module.exports = {
-	entry: {
-		bundle1: './main.js',
-		bundle2: ['./entry1.js','./entry2.js']
-	},
+	entry: './hello_world_main.js',
 	output: {
-		path: __dirname + '/bin',
-		filename: '[name].js'
+		filename: 'react_bundle.js'
 	},
 	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: 'babel',
-				query: {
-					presets: ['es2015']
-				}
-			}
-		]
+		loaders: [{
+			test: /\.jsx?$/,
+			loader: 'babel',
+			query: {
+				presets: ['es2015','react']
+			},
+			exclude: /node_modules/,
+		}]
 	}
 };
+
